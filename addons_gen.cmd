c3573f0b2de732a175a9012696e78dc6b2df5d91
Xycl <xycl2008@gmail.com>
2012-12-19 01:03:58 +0100

    some changes

---
+++ addons_gen.cmd
@@ -0,0 +1,6 @@
+copy ..\plugin.audio.listenliveeu\addon.xml plugin.audio.listenliveeu
+copy ..\plugin.image.mypicsdb\addon.xml plugin.image.mypicsdb
+copy ..\plugin.video.wdrrockpalast\addon.xml plugin.video.wdrrockpalast
+copy ..\script.module.dialogaddonscan\addon.xml script.module.dialogaddonscan
+
+c:\Python27\python.exe addons_gen.py
