39853e27376f6e677e48813020ea83173fbb1331
Xycl <xycl2008@gmail.com>
2014-05-26 14:35:06 +0200

    MyPicsDB fixed and new MyPicsDB Library Update Service

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -2,5 +2,6 @@ copy ..\plugin.audio.listenliveeu\addon.xml plugin.audio.listenliveeu
 copy ..\plugin.image.mypicsdb\addon.xml plugin.image.mypicsdb
 copy ..\plugin.video.wdrrockpalast\addon.xml plugin.video.wdrrockpalast
 copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
+copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
 
 c:\Python27\python.exe addons_gen.py
