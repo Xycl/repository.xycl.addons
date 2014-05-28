936c2def695cd237482e95dd7798a11dd43f7405
Xycl <xycl2008@gmail.com>
2014-05-28 11:23:26 +0200

    Amazon Prime Instant Video

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -3,6 +3,6 @@ copy ..\plugin.image.mypicsdb\addon.xml plugin.image.mypicsdb
 copy ..\plugin.video.wdrrockpalast\addon.xml plugin.video.wdrrockpalast
 rem copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
 copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
-copy ..\plugin.video.amazon_instant_prime_de\addon.xml plugin.video.amazon_instant_prime_de
+copy ..\plugin.video.amazon_prime_de\addon.xml plugin.video.amazon_prime_de
 
 c:\Python27\python.exe addons_gen.py
