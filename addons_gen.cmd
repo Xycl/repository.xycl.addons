8eac77c35340d099a0603ee7ef71b7608d32577d
Xycl <xycl2008@gmail.com>
2014-08-13 14:36:09 +0200

    Repository fixed

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -5,4 +5,4 @@ rem copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
 copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
 copy ..\plugin.video.amazon_prime_de\addon.xml plugin.video.amazon_prime_de
 
-c:\Python27\python.exe addons_gen.py
+C:\Apps\Python27\python.exe addons_gen.py
