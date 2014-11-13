f3b9adc0ff9ef3923c54736d58635d92f98c9ce6
Xycl <xycl2008@gmail.com>
2014-11-13 12:12:23 +0100

    New Amazon Prime Release

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -5,4 +5,4 @@ rem copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
 copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
 copy ..\plugin.video.amazon_prime_de\addon.xml plugin.video.amazon_prime_de
 
-C:\Apps\Python27\python.exe addons_gen.py
+C:\Python27\python.exe addons_gen.py
