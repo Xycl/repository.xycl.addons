53f63bd31fa4584e0e81cbbca6a4f29f2ed29812
Xycl <xycl2008@gmail.com>
2014-12-30 20:23:05 +0100

    Rockpalast fixed

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -1,7 +1,7 @@
 copy ..\plugin.audio.listenliveeu\addon.xml plugin.audio.listenliveeu
 copy ..\plugin.image.mypicsdb\addon.xml plugin.image.mypicsdb
 copy ..\plugin.video.wdrrockpalast\addon.xml plugin.video.wdrrockpalast
-rem copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
+copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
 copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
 copy ..\plugin.video.amazon_prime_de\addon.xml plugin.video.amazon_prime_de
 copy ..\plugin.audio.allartists\addon.xml plugin.audio.allartists
