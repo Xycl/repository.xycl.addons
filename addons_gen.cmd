6ca9263078f6229339075a5e1cd884474db37eae
Xycl <xycl2008@gmail.com>
2015-01-11 09:30:18 +0100

    Added Amazon UK

--- addons_gen.cmd
+++ addons_gen.cmd
@@ -3,7 +3,7 @@ copy ..\plugin.image.mypicsdb\addon.xml plugin.image.mypicsdb
 copy ..\plugin.video.wdrrockpalast\addon.xml plugin.video.wdrrockpalast
 rem copy ..\script.service.fritzbox\addon.xml script.service.fritzbox
 copy ..\script.service.mypicsdb\addon.xml script.service.mypicsdb
-copy ..\plugin.video.amazon_prime_de\addon.xml plugin.video.amazon_prime_de
+copy ..\plugin.video.xycl_amazon_prime\addon.xml plugin.video.xycl_amazon_prime
 copy ..\plugin.audio.allartists\addon.xml plugin.audio.allartists
 
 C:\Python27\python.exe addons_gen.py
